350c374bcf4dfd7ccc144bac69b993cd3cb085e2
Calm Andy <thought7878@qq.com>
2024-02-21 21:06:40 +0800

    feat: create actions structure

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,11 +1,4 @@
-'use server';
-
-import { signIn as sIn, signOut as sOut } from '@/auth';
-
-export async function signIn() {
-  return sIn('github');
-}
-
-export async function signOut() {
-  return sOut();
-}
+export { createComment } from './create-commnent';
+export { createPost } from './create-post';
+export { createTopic } from './create-topic';
+export { signIn, signOut } from './auth';

aa56bfa5da220ad2d447966d1107591f04591be6
Calm Andy <thought7878@qq.com>
2024-02-22 19:31:55 +0800

    feat(TopicShowPage): add

--- src/app/topics/[topicName]/page.tsx
+++ src/app/topics/[topicName]/page.tsx
@@ -1,3 +1,23 @@
-export default function TopicShowPage() {
-  return 'Topic show page';
+import { Button } from '@nextui-org/react';
+
+export interface TopicShowPageProps {
+  params: {
+    topicName: string;
+  };
+}
+
+export default function TopicShowPage({ params }: TopicShowPageProps) {
+  const { topicName } = params;
+  return (
+    <div className='grid grid-cols-4 gap-4 p-4'>
+      <div className='col-span-3'>
+        <h1 className='text-2xl font-bold mb-2'>{topicName}</h1>
+      </div>
+      <div>
+        <Button color='primary' type='submit'>
+          Create Post
+        </Button>
+      </div>
+    </div>
+  );
 }
